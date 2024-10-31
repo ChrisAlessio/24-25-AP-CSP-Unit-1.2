f9e3fdf4aecb1f44ac8d37a6439308f49592b5ed
13923 <Christopher.alessio@vtsd.com>
2024-10-31 11:43:04 -0400

    1-2-1 day one

--- 1-2-1/step3.py
+++ 1-2-1/step3.py
@@ -11,15 +11,18 @@ score = 0
 
 font_setup = ("Arial", 20, "normal")
 
-timer = 5
+timer = 30
 counter_interval = 1000   #1000 represents 1 second
 timer_up = False
 
+colors = ["red", "orange", "yellow", "green", "blue", "purple"]
+sizes = [0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0]
 #-----initialize turtle-----
 spot = trtl.Turtle()
 spot.shape(spot_shape)
 spot.shapesize(spot_size)
 spot.fillcolor(spot_color)
+spot.speed(5)
 
 score_writer = trtl.Turtle()
 
@@ -33,30 +36,13 @@ score_writer.goto(-350,350)
 counter.penup()
 counter.hideturtle()
 counter.goto(350,350)
-def update_score():
-    global score
-    score += 1
-    score_writer.clear()
-    score_writer.write(score,font=font_setup)
 
-
-def change_position():
-    spot.penup()
-    trtl.hideturtle()
-    new_xpos = rand.randint(-250,250)
-    new_ypos = rand.randint(-250,250)
-    spot.goto(new_xpos,new_ypos)
-
-def spot_clicked(x,y):
-    change_position()
-    update_score()
-    if timer_up == False:
-        update_score()
-        change_position()
-    else:
-        timer_up = True
-        spot.hideturtle()
-
+def adding_colors():
+    spot.color(rand.choice(colors))
+    spot.stamp()
+    spot.color("pink")
+def changing_size():
+    spot.shapesize(rand.choice(sizes))
 def countdown():
   global timer, timer_up
   counter.clear()
@@ -67,9 +53,30 @@ def countdown():
     counter.write("Timer: " + str(timer), font=font_setup)
     timer -= 1
     counter.getscreen().ontimer(countdown, counter_interval)
+def update_score():
+    global score
+    score += 1
+    score_writer.clear()
+    score_writer.write(score,font=font_setup)
+def change_position():
+    spot.penup()
+    trtl.hideturtle()
+    new_xpos = rand.randint(-250,250)
+    new_ypos = rand.randint(-250,250)
+    spot.goto(new_xpos,new_ypos)
+def spot_clicked(x,y):
+    if timer_up == False:
+        changing_size()
+        adding_colors()
+        update_score()
+        change_position()
+    else:
+        timer_up == True
+        spot.hideturtle()
 #-----events----------------
 spot.onclick(spot_clicked)
 
 wn = trtl.Screen()
+wn.bgcolor("skyblue")
 wn.ontimer(countdown, counter_interval)
 wn.mainloop()
